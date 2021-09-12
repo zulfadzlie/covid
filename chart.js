bb0892f21101af322285627a6b54be37b662c460
zulfadzlie <55380804+zulfadzlie@users.noreply.github.com>
2021-09-12 16:59:53 +0800

    Add files via upload
    Added new deaths per million data query

--- chart.js
+++ chart.js
@@ -1,4 +1,6 @@
-var trace1 = {
+
+
+let trace1 = {
     x: chart_vaccinated,
     y: chart_cases,
     mode: 'markers+text',
@@ -11,11 +13,11 @@ var trace1 = {
     },
     marker: {
         size: 12,
-        color: "#04AA6D"
+        color: "#0075FF"
     }
 }
 
-var trace2 = {
+let trace2 = {
     x: chart_vaccinated_days_ago,
     y: chart_cases_days_ago,
     mode: 'markers+text',
@@ -28,13 +30,50 @@ var trace2 = {
     },
     marker: {
         size: 12,
-        color: "rgb(128, 128, 128)"
+        color: "rgb(200, 200, 200)"
     }
 }
 
-var data = [ trace1, trace2 ]
-  
-var layout = {
+let trace3 = {
+    x: chart_vaccinated,
+    y: chart_deaths,
+    mode: 'markers+text',
+    type: 'scatter',
+    name: "Latest",
+    text: chart_countries,
+    textposition: 'right center',
+    textfont: {
+      family:  'Raleway, sans-serif'
+    },
+    marker: {
+        size: 12,
+        color: "#0075FF"
+    }
+}
+
+let trace4 = {
+    x: chart_vaccinated_days_ago,
+    y: chart_deaths_days_ago,
+    mode: 'markers+text',
+    type: 'scatter',
+    name: days_ago + " days ago",
+    text: chart_countries_days_ago,
+    textposition: 'right center',
+    textfont: {
+      family:  'Raleway, sans-serif'
+    },
+    marker: {
+        size: 12,
+        color: "rgb(200, 200, 200)"
+    }
+}
+
+
+let chart_data = [ trace1, trace2 ]
+
+let chart2_data = [ trace3, trace4 ]
+
+let layout = {
     xaxis: {
         title: "People fully vaccinated per 100",
         range: [ 0, 100 ],
@@ -44,7 +83,13 @@ var layout = {
     yaxis: {
         title: "New cases smoothed per million",
         dtick: 100
-        // range: [0, 700]
+    },
+
+    margin: {
+        t: 0,
+        r: 0,
+        l: 50,
+        b: 50
     },
 
     legend: {
@@ -52,15 +97,46 @@ var layout = {
         bordercolor: "#E2E2E2",
         borderwidth: 1,
         x: 0.5,
-        y: 1.15,
+        y: 1.1,
         xanchor: "center"
-
     }
 }
 
-var config = {
+let layout2 = {
+    xaxis: {
+        title: "People fully vaccinated per 100",
+        range: [ 0, 100 ],
+        dtick: 10
+    },
+
+    yaxis: {
+        title: "New deaths smoothed per million",
+        dtick: 1
+    },
+
+    margin: {
+        t: 0,
+        r: 0,
+        l: 50,
+        b: 50
+    },
+
+    legend: {
+        "orientation": "h",
+        bordercolor: "#E2E2E2",
+        borderwidth: 1,
+        x: 0.5,
+        y: 1.1,
+        xanchor: "center"
+    }
+}
+
+let config = {
     responsive: true,
     displayModeBar: false
 }
 
-// Plotly.newPlot('myDiv', data, layout)
+let config2 = {
+    responsive: true,
+    displayModeBar: false
+}
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+let json_data = []
+
 let countries = [
     "MYS", 
     "BRN", 
@@ -10,16 +13,14 @@ let countries = [
 
 let chart_vaccinated = []
 let chart_cases = []
+let chart_deaths = []
 let chart_countries = []
 
 let chart_vaccinated_days_ago = []
 let chart_cases_days_ago = []
+let chart_deaths_days_ago = []
 let chart_countries_days_ago = []
 
-let to_print_location = ""
-let to_print_dates = ""
-let to_print_vaccinated = ""
-let to_print_cases = ""
 let latest_date =""
 
 let adjust = 1
@@ -42,13 +43,16 @@ request.onload = function() {
         let date = data[country_code].data[data[country_code].data.length - adjust].date
         let vaccinated = findLatestVaccinated(country_data, adjust)
         let cases = data[country_code].data[data[country_code].data.length - adjust].new_cases_smoothed_per_million
-        write(location, date, vaccinated, cases, "latest")
+        let deaths = data[country_code].data[data[country_code].data.length - adjust].new_deaths_smoothed_per_million
+        write(location, date, vaccinated, cases, deaths, "latest")
 
         let date_days_ago = data[country_code].data[data[country_code].data.length - days_ago].date
         let vaccinated_days_ago = findLatestVaccinated(country_data, days_ago)
         let cases_days_ago = data[country_code].data[data[country_code].data.length - days_ago].new_cases_smoothed_per_million
-        write(location, date_days_ago, vaccinated_days_ago, cases_days_ago, "days_ago")
-    }
+        let deaths_days_ago = data[country_code].data[data[country_code].data.length - days_ago].new_deaths_smoothed_per_million
+        write(location, date_days_ago, vaccinated_days_ago, cases_days_ago, deaths_days_ago,"days_ago")
+        json_data[country_code] = country_data
+    }    
 }
 
 function findLatestVaccinated(country_data, start_date) {
@@ -63,20 +67,42 @@ function findLatestVaccinated(country_data, start_date) {
     return vaccinated
 }
 
-function write(location, date, vaccinated, cases, which_one) {
+function write(location, date, vaccinated, cases, deaths, which_one) {
     if (which_one === "latest") {
         chart_vaccinated.push(vaccinated)
         chart_cases.push(cases)
+        chart_deaths.push(deaths)
         chart_countries.push(location)
         latest_date = date 
     } else {
         chart_vaccinated_days_ago.push(vaccinated)
         chart_cases_days_ago.push(cases)
+        chart_deaths_days_ago.push(deaths)
         chart_countries_days_ago.push(location)
     }
 
-    Plotly.newPlot('myDiv', data, layout, config)
+    Plotly.newPlot('myDiv', chart_data, layout, config)
+    // Plotly.newPlot('myDiv2', chart2_data, layout2, config2)
     document.getElementById("date").textContent = latest_date
 }
 
-console.log(chart_countries)
+// console.log(json_data)
+
+// function radioSelected(selected) {
+//     if (selected === "Deaths") {
+//         document.getElementById("chart-container2").classList.remove("hidden")
+//         document.getElementById("chart-container").classList.add("hidden")
+
+//     } else {
+//         document.getElementById("chart-container").classList.remove("hidden")
+//         document.getElementById("chart-container2").classList.add("hidden")
+//     }
+// }
+
+function radioSelected(selected) {
+    if (selected === "Deaths") {
+        Plotly.newPlot('myDiv', chart2_data, layout2, config2)
+    } else {
+        Plotly.newPlot('myDiv', chart_data, layout, config)
+    }
+}
